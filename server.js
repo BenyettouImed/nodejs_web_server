0366d29a2e0af24e5cae49245147a7dbbe2a36ec
BenyettouImed <imed.benny@gmail.com>
2026-01-24 12:26:25 +0100

    cotentType

--- server.js
+++ server.js
@@ -11,6 +11,44 @@ const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    res.end('Hello World!');
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+        case '.jpeg':
+            contentType = 'image/jpeg';
+            break;
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/plain';
+            break;
+    }
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
 /* myEmitter.on('log', (message) => logEvents(message, 'log.txt'));
 
 setTimeout(() => {
