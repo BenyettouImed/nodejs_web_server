8d1422a6234b3d40df46c874f72c6d950b1720c2
BenyettouImed <imed.benny@gmail.com>
2026-01-25 17:49:49 +0100

    routing with express js

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.get(/^\/(|index(.html)?)$/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get(/\/new-page(.html)?/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+})
+app.get(/\/old-page(.html)?/, (req, res) => {
+    res.redirect(301, path.join(__dirname, 'views', 'new-page.html')) // 302 by default which means temporary redirect
+    // 301 means permanent redirect
 })
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT))
